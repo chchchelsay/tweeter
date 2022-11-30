9630a070ddfb50017336aa80ea38ac2514ffc5c5
Chelsea Dwarika <chelseakristina@live.com>
2022-11-30 17:20:59 +0000

    eslint char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
-  $("#tweet-text").on('input', function () {
+// this applies maxChars class to make counter turn blue-red when over 140 chars
+
+$(document).ready(function() {
+  $("#tweet-text").on('input', function() {
     const tweet = $('#tweet-text').val();
     const result = (140 - tweet.length);
     $('.counter').text(result).toggleClass('maxChars', result < 0);
